a12cde3ebc7e62e3241829606c03d638cb39244b
liangchao <kenliang456852@163.com>
2018-07-20 15:33:51 +0800

    realm

---
+++ integrated-project-shiros/src/main/java/com/integrated/shiros/realm/CustomRealm.java
@@ -0,0 +1,13 @@
+package com.integrated.shiros.realm;
+
+/**
+ * ClassName: CustomRealm
+ * Description:
+ * Author: liangchao
+ * Date: 2018/7/20 15:33
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * liangc           修改时间           0.0.1              描述
+ */
+public class CustomRealm {
+}
