1ee285ffbc8e3b565cdc0b456aebd2d79d0a7054
kenliang456852 <kenliang456852@163.com>
2018-08-14 17:33:43 +0800

    shiroing

--- integrated-project-shiros/src/main/resource/static/js/index.js
+++ integrated-project-shiros/src/main/resource/static/js/index.js
@@ -1,6 +1,7 @@
 $(function() {
-
-    for(var i = 1; i < 13; i ++) {
+    for(var i = 2; i < 13; i ++) {
+        var tr1 = $('#bodyT').find('tr').eq(0).clone();
+        tr1.find('td').eq(0).text(i)
+        $('#bodyT').append(tr1);
     }
-    $('#bodyT').html(content);
 });
